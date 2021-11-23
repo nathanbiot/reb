6739b97d48610f1420c7e19b1ca530fd20cc4662
nbi <biot.nathan@gmail.com>
2021-11-23 16:10:07 +0100

    Android Splashscreen

--- src/reb/reb.Android/MainActivity.cs
+++ src/reb/reb.Android/MainActivity.cs
@@ -5,7 +5,7 @@ using Shiny;
 [assembly: ShinyApplication(XamarinFormsAppTypeName = "reb.App", ShinyStartupTypeName = "reb.Startup")]
 namespace reb.Droid
 {
-    [Activity(Label = "reb", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "reb", Icon = "@mipmap/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public partial class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
     }
